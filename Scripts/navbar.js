708e73f380b760dae095fe58e1120fe24bc00920
MikeParkinWits <80671326+MikeParkinWits@users.noreply.github.com>
2021-05-18 08:46:01 +0200

    Create navbar.js

---
+++ Scripts/navbar.js
@@ -0,0 +1,47 @@
+const menuOptions = [
+    {title: 'Portfolio', link: '../PortfolioPage/PortfolioMainPage.html'},
+    {title: 'Blog', link: '../Blogs/AllBlogsP1.html'},
+    {title: 'About Me', link: '../AboutPage/AboutMePage.html'},
+    {title: 'Contact Me', link: '../ContactPage/ContactMePage.html'}
+
+]
+
+const initialiseMenuLogo = (ul) => {
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = '../index.html';
+    const image = document.createElement('img');
+    image.src = './Images/Logo.png';
+    image.width = 200;
+    image.alt = 'Jake Sky Logo';
+    link.appendChild(image);
+
+    li.appendChild(link);
+
+    ul.appendChild(li);
+}
+
+const initialiseMenu = () =>{
+    const nav = document.querySelector('.header-nav');
+
+    const ul = document.createElement('ul');
+
+    initialiseMenuLogo(ul);
+
+    for (let option of menuOptions){
+        const li = document.createElement('li');
+        const link = document.createElement('a');
+        link.innerText = option.title;
+        link.href = option.link;
+
+        li.appendChild(link);
+
+        ul.appendChild(li);
+    }
+
+    nav.appendChild(ul);
+};
+
+
+
+document.addEventListener('DOMContentLoaded', () => initialiseMenu());
